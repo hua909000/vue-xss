61fc1087362b191ec8ba38c327bf3a2d859de491
hua909000 <zhoushunhua@codemao.cn>
2019-10-20 20:45:19 +0800

    feat: add options

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 import xss from 'xss'
 
 export default {
-  install: function (Vue) {
+  install: function (Vue, options = {}) {
     Object.defineProperty(Vue.prototype, '$xss', {
       value: function (html) {
-        return xss(html)
+        return xss(html, options)
       }
     })
   }
