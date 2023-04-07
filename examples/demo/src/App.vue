1c3dc779abe6be0c934cd27fdd09e3b24c3d62a9
hua909000 <zhoushunhua@codemao.cn>
2023-04-07 11:37:10 +0800

    chore: update demo

--- examples/demo/src/App.vue
+++ examples/demo/src/App.vue
@@ -2,10 +2,12 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
 
+    <p>before</p>
     <div v-html="content"></div>
 
-    <!-- <div v-html="$xss(content)"></div> -->
-    <!-- <iframe onload=alert('XSS-TEST')> 过滤后输出为：<iframe></iframe> -->
+    <p>after</p>
+    <div v-html="$xss(filterContent)"></div>
+    <!-- <iframe onload=alert('after-XSS-TEST')> 过滤后输出为：<iframe></iframe> -->
   </div>
 </template>
 
@@ -13,8 +15,8 @@
 export default {
   data () {
     return {
-      content: '<iframe onload=alert("XSS-TEST")></iframe>',
-      // content: '<iframe src=javascript:alert("XSS-TEST")></iframe>'
+      content: '<iframe onload=alert("before-XSS-TEST")></iframe>',
+      filterContent: '<iframe src=javascript:alert("after-XSS-TEST")></iframe>'
     }
   }
 }
